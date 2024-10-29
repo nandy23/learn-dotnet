c9b30e5608c979fb3e4db12d061c7dcbd39b99e9
pupunn <pupunn@students.unnes.ac.id>
2024-10-29 22:13:27 +0700

    add validation and filters

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4b7ec44d9246a805821ff362a0af186394647aa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67b54e7e9a9f1aa3342a6843bd28995bce6c6583")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
