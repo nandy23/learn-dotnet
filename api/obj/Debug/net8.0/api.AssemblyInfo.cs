7396c6af1e6cf5dee944362037302326406b28c1
pupunn <pupunn@students.unnes.ac.id>
2024-10-29 22:49:09 +0700

    Sorting and Pagination

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67b54e7e9a9f1aa3342a6843bd28995bce6c6583")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9b30e5608c979fb3e4db12d061c7dcbd39b99e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
